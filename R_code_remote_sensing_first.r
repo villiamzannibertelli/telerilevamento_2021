9d339e08f14d07141dc2b75435fc23992b8c5c31
villiamzannibertelli <80513540+villiamzannibertelli@users.noreply.github.com>
2021-03-24 12:51:06 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -27,3 +27,27 @@ plot(p224r63_2011)
 > #cambio i colori 
 > cl <- colorRampPalette (c('pink', 'magenta', 'yellow', 'light pink')) (100)
 > plot(p224r63_2011, col=cl)
+#
+
+> p224r63_2011
+class      : RasterBrick 
+dimensions : 1499, 2967, 4447533, 7  (nrow, ncol, ncell, nlayers)
+resolution : 30, 30  (x, y)
+extent     : 579765, 668775, -522705, -477735  (xmin, xmax, ymin, ymax)
+crs        : +proj=utm +zone=22 +datum=WGS84 +units=m +no_defs 
+source     : /Users/villiamzannibertelli/lab/p224r63_2011_masked.grd 
+names      :       B1_sre,       B2_sre,       B3_sre,       B4_sre,       B5_sre,        B6_bt,       B7_sre 
+min values : 0.000000e+00, 0.000000e+00, 0.000000e+00, 1.196277e-02, 4.116526e-03, 2.951000e+02, 0.000000e+00 
+max values :    0.1249041,    0.2563655,    0.2591587,    0.5592193,    0.4894984,  305.2000000,    0.3692634 
+
+> #bande landsat 
+> # B1: blu 
+> # B2: verde 
+> # B3: rosso 
+> # B4: NIR (vicino infrarosso)
+> # B5: SWIR (medio infrarosso)
+> # B6: TIR (infrarosso termico)
+> # B7: SWIR (medio infrarosso)
+> 
+#ripulisce la finestra grafica 
+dev.off()
