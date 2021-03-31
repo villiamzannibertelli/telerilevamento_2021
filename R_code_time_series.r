afb58f33c9fedda90cecfd0662cf3a439702d07e
villiamzannibertelli <80513540+villiamzannibertelli@users.noreply.github.com>
2021-03-31 13:01:55 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,8 @@
+#time series analysis 
+#aumento della temperatura in Groenlandia 
+#data and code from Emanuela Cosma 
 
+#si utilizza sempre la libreria raster
+library(raster)
+
+setwd('/Users/villiamzannibertelli/lab/greenland/')
