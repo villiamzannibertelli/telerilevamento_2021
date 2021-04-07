1924be299c5fabb868644ed662cb4273f095d823
villiamzannibertelli <80513540+villiamzannibertelli@users.noreply.github.com>
2021-04-07 13:07:41 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -6,3 +6,29 @@
 library(raster)
 
 setwd('/Users/villiamzannibertelli/lab/greenland/')
+
+#creiamo il raster utilizzando la funzione 'raster' e assegnamo un nome 
+
+lst_2000 <- raster("lst_2000.tif")
+
+plot(lst_2000)
+
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+#Facciamo un par 
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+#list of files: 
+
+rlist <- list.files(pattern='lst')
+#applichiamo la funzione lapply 
+import <- lapply(rlist,raster)
+
+#crea un'unica immagine raster composta dall'insieme delle immagini singole
+TGr <- stack(import)
